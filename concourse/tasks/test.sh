c19be6adf9f46bb1ac257444cc5fef9748a1aacc
mfinger <mfinger@mastercontrol.com>
2019-11-21 14:56:45 -0700

    foo

--- concourse/tasks/test.sh
+++ concourse/tasks/test.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 set -e
-spring-boot-concourse/gradlew clean test
+spring-boot-concourse/gradlew --build-file spring-boot-concourse/build.gradle clean test 
 cp spring-boot-concourse/build/libs/*.jar build-output/
