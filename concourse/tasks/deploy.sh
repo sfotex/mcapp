57f2633d09c60564497e67caeecc0031ddd10d60
mfinger <mfinger@mastercontrol.com>
2019-11-21 16:51:58 -0700

    foo

--- concourse/tasks/deploy.sh
+++ concourse/tasks/deploy.sh
@@ -1,3 +1,13 @@
-#!/bin/sh
+#!/bin/bash
 set -e
+
+export REPO=repo
+GRADLE_HOME="${HOME}/.gradle"
+GRADLE_CACHE="${ROOT_FOLDER}/gradle"
+ 
+echo "Generating symbolic links for caches"
+
+[[ -d "${GRADLE_CACHE}" && ! -d "${GRADLE_HOME}" ]] && ln -s "${GRADLE_CACHE}" "${GRADLE_HOME}"
+
+
 spring-boot-concourse/gradlew --build-file spring-boot-concourse/build.gradle clean test
